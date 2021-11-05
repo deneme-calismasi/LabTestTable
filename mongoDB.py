0d2efe7e9c0e5236f2fd5de21a8568ec6fb211aa
halilerhan.orun <erhan0494@gmail.com>
2021-11-05 11:29:27 +0300

    05.11.2021 11:30

--- mongoDB.py
+++ mongoDB.py
@@ -2,7 +2,7 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["Modbus_Database"]
-mycol = mydb["collection4"]
+mycol = mydb["collection5"]
 
 for x in mycol.find():
     print(x)
