60f19698ee1b35f54e7f2c0e77f913038e18fc3d
halilerhan.orun <erhan0494@gmail.com>
2022-01-19 10:19:32 +0300

    19.01.2022 10:20

---
+++ mongo_lab_L3.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["Modbus_Database"]
+mycol = mydb["collection7"]
+
+# data = pd.DataFrame(mycol.find({}, {'_id': 0}))
+data = pd.DataFrame(
+    mycol.find({"Sensor Type No": "3"}, {'_id': 0}))
+
+data.to_csv('Lab_Data_L3.csv')
+
+read_file = pd.read_csv(r'Lab_Data_L3.csv')
+read_file.to_excel(r'Lab_Data_Excel_L3.xlsx', index=None, header=True)
